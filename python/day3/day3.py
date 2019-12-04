186a3eceb2f6c2f867da642aa279864cd33f22ab
Frerich Raabe <raabe@froglogic.com>
2019-12-04 14:13:47 +0100

    Cleaned up Day 3 in Python a bit

--- python/day3/day3.py
+++ python/day3/day3.py
@@ -1,24 +1,21 @@
 def steps(line):
+    step = {
+        'U': ( 0,-1),
+        'D':  (0, 1),
+        'L': (-1, 0),
+        'R': ( 1, 0),
+    }
     for segment in line.split(","):
-        if segment[0] == 'U':
-            delta = (0, -1)
-        elif segment[0] == 'D':
-            delta = (0, 1)
-        elif segment[0] == 'L':
-            delta = (-1, 0)
-        elif segment[0] == 'R':
-            delta = (1, 0)
-
-        yield from [delta] * int(segment[1:])
+        yield from [step[segment[0]]] * int(segment[1:])
 
 
 def wire(line):
     x, y = 0, 0
     for dx, dy in steps(line):
-        yield (x, y)
+        yield x, y
         x += dx
         y += dy
-    yield (x, y)
+    yield x, y
 
 
 def manhattan(pos):
