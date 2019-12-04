c628f459ba90c021f494ce45ac5727b0e3f18559
Frerich Raabe <raabe@froglogic.com>
2019-12-04 09:55:07 +0100

    Cleaned up Haskell solution for Day 4

--- haskell/day4/day4.hs
+++ haskell/day4/day4.hs
@@ -1,13 +1,4 @@
-import Control.Monad (guard)
 import Data.List (group)
-
-
-decimal :: [Int] -> Int
-decimal digits = sum (zipWith (\n p -> n * 10 ^ p) (reverse digits) [0..])
-
-
-candidates :: [[Int]]
-candidates = [[a,b,c,d,e,f] | a <- [1..9], b <- [a..9], c <- [b..9], d <- [c..9], e <- [d..9], f <- [e..9]]
 
 
 hasRepetition :: Eq a => [a] -> Bool
@@ -18,12 +9,19 @@ hasDouble :: Eq a => [a] -> Bool
 hasDouble xs = any (\g -> length g == 2) (group xs)
 
 
-solutions :: ([Int] -> Bool) -> Int
-solutions pred = length . dropWhile (< 130254) . takeWhile (<= 678275) . map decimal . filter pred $ candidates
+solutions :: [[Int]] -> Int
+solutions = length . dropWhile (< 130254) . takeWhile (<= 678275) . map decimal
+  where
+    decimal digits = sum (zipWith (\n p -> n * 10 ^ p) (reverse digits) [0..])
 
 
 main :: IO ()
 main = do
-    putStrLn ("Part one: " ++ show (solutions hasRepetition))
-    putStrLn ("Part two: " ++ show (solutions hasDouble))
+    let candidates = [[a,b,c,d,e,f] | a <- [1..9], b <- [a..9], c <- [b..9], d <- [c..9], e <- [d..9], f <- [e..9]]
+
+    let withRepetitions = filter hasRepetition candidates
+    let withDoubles = filter hasDouble withRepetitions
+
+    putStrLn ("Part one: " ++ show (solutions withRepetitions))
+    putStrLn ("Part two: " ++ show (solutions withDoubles))
 
