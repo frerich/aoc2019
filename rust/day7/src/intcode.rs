fd7cb7f20fb27f930cd8491ebdc256f00398ef25
Frerich Raabe <raabe@froglogic.com>
2019-12-07 19:57:01 +0100

    Solution for Day 7 part 2 in Rust (phew!)

--- rust/day7/src/intcode.rs
+++ rust/day7/src/intcode.rs
@@ -3,9 +3,9 @@ pub fn parse(input: &str) -> Option<Vec<isize>> {
 }
 
 
-fn decode_arg(mem: &[isize], ip: usize, idx: u32) -> isize {
-    let opcode = mem[ip];
-    let arg = mem[ip + 1 + idx as usize];
+fn decode_arg(mem: &[isize], ip: &usize, idx: u32) -> isize {
+    let opcode = mem[*ip];
+    let arg = mem[*ip + 1 + idx as usize];
     if (opcode % 10_isize.pow(idx + 3)) / 10_isize.pow(idx + 2) == 1 {
         arg
     } else {
@@ -14,78 +14,82 @@ fn decode_arg(mem: &[isize], ip: usize, idx: u32) -> isize {
 }
 
 
-pub fn run(mem: &mut [isize], mut put: impl FnMut(isize), mut get: impl Iterator<Item = isize>) {
-    let mut ip = 0;
-    loop {
-        match mem[ip] % 100 {
-            1 => {
-                let arg_0 = decode_arg(&mem, ip, 0);
-                let arg_1 = decode_arg(&mem, ip, 1);
-                let arg_2 = mem[ip + 3];
-                mem[arg_2 as usize] = arg_0 + arg_1;
-                ip += 4;
-            }
-            2 => {
-                let arg_0 = decode_arg(&mem, ip, 0);
-                let arg_1 = decode_arg(&mem, ip, 1);
-                let arg_2 = mem[ip + 3];
-                mem[arg_2 as usize] = arg_0 * arg_1;
-                ip += 4;
-            } 
-            3 => {
-                let arg_0 = mem[ip + 1];
-                mem[arg_0 as usize] = get.next().expect("Failed to read input value");
-                ip += 2;
-            }
-            4 => {
-                let arg_0 = decode_arg(&mem, ip, 0);
-                put(arg_0);
-                ip += 2;
-            }
-            5 => {
-                let arg_0 = decode_arg(&mem, ip, 0);
-                if arg_0 != 0 {
-                    ip = decode_arg(&mem, ip, 1) as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            6 => {
-                let arg_0 = decode_arg(&mem, ip, 0);
-                if arg_0 == 0 {
-                    ip = decode_arg(&mem, ip, 1) as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            7 => {
-                let arg_0 = decode_arg(&mem, ip, 0);
-                let arg_1 = decode_arg(&mem, ip, 1);
-                let arg_2 = mem[ip + 3];
-                if arg_0 < arg_1 {
-                    mem[arg_2 as usize] = 1;
-                } else {
-                    mem[arg_2 as usize] = 0;
-                }
-                ip += 4
-            }
-            8 => {
-                let arg_0 = decode_arg(&mem, ip, 0);
-                let arg_1 = decode_arg(&mem, ip, 1);
-                let arg_2 = mem[ip + 3];
-                if arg_0 == arg_1 {
-                    mem[arg_2 as usize] = 1;
-                } else {
-                    mem[arg_2 as usize] = 0;
-                }
-                ip += 4
-            }
-            99 => {
-                break
-            }
-            n => {
-                panic!("Unexpected opcode: {:?}", n);
+pub fn step(mem: &mut [isize], ip: &mut usize, put: &mut impl FnMut(isize), get: &mut impl FnMut() -> isize) -> u8 {
+    let opcode = (mem[*ip] % 100) as u8;
+    match opcode {
+        1 => {
+            let arg_0 = decode_arg(&mem, ip, 0);
+            let arg_1 = decode_arg(&mem, ip, 1);
+            let arg_2 = mem[*ip + 3];
+            mem[arg_2 as usize] = arg_0 + arg_1;
+            *ip += 4;
+        }
+        2 => {
+            let arg_0 = decode_arg(&mem, ip, 0);
+            let arg_1 = decode_arg(&mem, ip, 1);
+            let arg_2 = mem[*ip + 3];
+            mem[arg_2 as usize] = arg_0 * arg_1;
+            *ip += 4;
+        } 
+        3 => {
+            let arg_0 = mem[*ip + 1];
+            mem[arg_0 as usize] = get();
+            *ip += 2;
+        }
+        4 => {
+            let arg_0 = decode_arg(&mem, ip, 0);
+            put(arg_0);
+            *ip += 2;
+        }
+        5 => {
+            let arg_0 = decode_arg(&mem, ip, 0);
+            if arg_0 != 0 {
+                *ip = decode_arg(&mem, ip, 1) as usize;
+            } else {
+                *ip += 3;
             }
         }
+        6 => {
+            let arg_0 = decode_arg(&mem, ip, 0);
+            if arg_0 == 0 {
+                *ip = decode_arg(&mem, ip, 1) as usize;
+            } else {
+                *ip += 3;
+            }
+        }
+        7 => {
+            let arg_0 = decode_arg(&mem, ip, 0);
+            let arg_1 = decode_arg(&mem, ip, 1);
+            let arg_2 = mem[*ip + 3];
+            if arg_0 < arg_1 {
+                mem[arg_2 as usize] = 1;
+            } else {
+                mem[arg_2 as usize] = 0;
+            }
+            *ip += 4
+        }
+        8 => {
+            let arg_0 = decode_arg(&mem, ip, 0);
+            let arg_1 = decode_arg(&mem, ip, 1);
+            let arg_2 = mem[*ip + 3];
+            if arg_0 == arg_1 {
+                mem[arg_2 as usize] = 1;
+            } else {
+                mem[arg_2 as usize] = 0;
+            }
+            *ip += 4
+        }
+        99 => {
+        }
+        n => {
+            panic!("Unexpected opcode: {:?}", n);
+        }
+    }
+    opcode
+}
+
+pub fn run(mem: &mut [isize], mut put: impl FnMut(isize), mut get: impl FnMut() -> isize) {
+    let mut ip = 0;
+    while step(mem, &mut ip, &mut put, &mut get) != 99 {
     }
 }
--- rust/day7/src/main.rs
+++ rust/day7/src/main.rs
@@ -1,24 +1,90 @@
 extern crate permutohedron;
 
+use std::collections::VecDeque;
+use crate::permutohedron::LexicalPermutation;
+use std::iter::FromIterator;
+
 mod intcode;
 
-fn run_amplifiers(ints: &[isize], phase_signals: &[isize]) -> isize {
-    let mut input: isize = 0;
-    for phase in phase_signals {
-        let mut mem = ints.to_vec();
-        let inputs = vec![*phase, input];
-        intcode::run(&mut mem, |val| input = val, inputs.into_iter());
+struct Amplifier {
+    ip: usize,
+    mem: Vec<isize>,
+    input: VecDeque<isize>
+}
+
+fn run(amp: &mut Amplifier) -> Option<isize> {
+    let mut output = None;
+    let mut inputs = amp.input.clone();
+    loop {
+        let opcode = intcode::step(
+            &mut amp.mem,
+            &mut amp.ip,
+            &mut |val| output = Some(val),
+            &mut || inputs.pop_front().expect("Missing input")
+            );
+
+        if opcode == 4 || opcode == 99 {
+            break;
+        }
     }
-    input
+    amp.input = inputs;
+    output
+}
+
+fn sequence(amplifiers: &mut [Amplifier], first_input: isize) -> Option<isize> {
+    let mut input = first_input;
+    for mut amp in amplifiers {
+        amp.input.push_back(input);
+        match run(&mut amp) {
+            Some(output) => input = output,
+            None         => return None
+        };
+    }
+    Some(input)
+}
+
+fn find_maximum_thrust_by(amplifier_program: &[isize], phase_signals: &[isize], go: impl Fn(&mut [Amplifier]) -> isize) -> isize {
+    let mut max_thrust = 0;
+
+    let mut phase_permutation = phase_signals.to_vec();
+
+    loop {
+        let mut amplifiers = phase_permutation.iter().map(|phase_signal| {
+            Amplifier {
+                ip: 0,
+                mem: amplifier_program.to_vec(),
+                input: VecDeque::from_iter(vec![*phase_signal]),
+            }
+        }).collect::<Vec<_>>();
+
+        max_thrust = std::cmp::max(max_thrust, go(&mut amplifiers));
+
+        if !phase_permutation.next_permutation() {
+            break;
+        }
+    }
+
+    max_thrust
 }
 
 fn part_one(ints: &[isize]) -> isize {
-    let mut phase_signals = vec![0,1,2,3,4];
-    permutohedron::Heap::new(&mut phase_signals)
-        .into_iter()
-        .map(|phase_permutation| run_amplifiers(&ints, &phase_permutation))
-        .max()
-        .unwrap()
+    let phase_signals: Vec<_> = (0..5).collect();
+    find_maximum_thrust_by(ints, &phase_signals, |mut amplifiers| {
+        sequence(&mut amplifiers, 0).unwrap_or(0)
+    })
+}
+
+fn part_two(ints: &[isize]) -> isize {
+    let phase_signals: Vec<_> = (5..10).collect();
+    find_maximum_thrust_by(ints, &phase_signals, |mut amplifiers| {
+        let mut input = 0;
+        loop {
+            match sequence(&mut amplifiers, input) {
+                Some(thrust) => input = thrust,
+                None         => return input
+            };
+        }
+    })
 }
 
 fn main() {
@@ -29,4 +95,5 @@ fn main() {
         .expect("Failed to parse input file");
 
     println!("Part one: {:?}", part_one(&ints));
+    println!("Part two: {:?}", part_two(&ints));
 }
