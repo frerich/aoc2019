2d82a39d5c96374b9bc61c61a283e7284b392195
Frerich Raabe <raabe@froglogic.com>
2019-12-17 10:39:26 +0100

    Day 17 part 1 in Rust

---
+++ rust/day17/src/main.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+
+mod intcode;
+
+
+fn part_one(program: &[isize]) -> usize {
+    let mut scaffolds = HashSet::new();
+    let mut x = 0;
+    let mut y = 0;
+    let mut process = intcode::Process::new(program);
+    process.run(
+        |ascii| match (ascii as u8) as char {
+            '#'  => { scaffolds.insert((x,y)); x += 1; },
+            '\n' => { x = 0; y += 1; },
+            _    => x += 1
+        },
+        || 0
+    );
+
+    scaffolds.iter()
+        .filter(|&(x,y)|
+            scaffolds.contains(&(x - 1, *y)) &&
+            scaffolds.contains(&(x + 1, *y)) &&
+            scaffolds.contains(&(*x, y - 1)) &&
+            scaffolds.contains(&(*x, y + 1))
+        )
+        .map(|(x, y)| (x * y) as usize)
+        .sum()
+}
+
+
+fn main() {
+    let input = std::fs::read_to_string("input.txt")
+        .expect("Failed to read input.txt");
+
+    let program = intcode::parse(input.trim_end())
+        .expect("Failed to parse input file");
+
+    println!("Part one: {}", part_one(&program));
+}
