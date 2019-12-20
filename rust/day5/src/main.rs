5660d83cc2d34726cef11ba0f86eb5d209820223
Frerich Raabe <raabe@froglogic.com>
2019-12-20 09:25:59 +0100

    Ported Day 5 solution of Rust to shared intcode library

--- rust/day5/src/main.rs
+++ rust/day5/src/main.rs
@@ -1,11 +1,26 @@
-mod intcode;
+use intcode;
 
-fn main() -> std::io::Result<()> {
+
+fn output(program: &[isize], input: isize) -> isize {
+    let mut output  = 0;
+
+    let mut process = intcode::Process::new(program);
+    process.run(
+        |val| output = val,
+        || input
+    );
+
+    output
+}
+
+
+fn main() {
     let input = std::fs::read_to_string("input.txt")
         .expect("Failed to read input.txt");
 
-    let mut ints = intcode::parse(&input)
+    let program = intcode::parse(input.trim_end())
         .expect("Failed to parse input file");
 
-    intcode::run(&mut ints).map(|_| ())
+    println!("Part one: {}", output(&program, 1));
+    println!("Part two: {}", output(&program, 5));
 }
